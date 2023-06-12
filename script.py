36d976646055dcb6309b7cf3a41b0c3b3b176f07
Ele975 <55561956+Ele975@users.noreply.github.com>
2023-06-12 21:32:10 +0200

    line intersection doesn't work, comment code
    create hull space with constant knowing that position of images are the same

--- script.py
+++ script.py
@@ -97,137 +97,63 @@ def create_img(img_name, img, dic, rot_y):
 # Create light rays -> use a mesh and attach it to an object, then insert in collection
 # create a ray for each pixel center
 
-# # input: grid of binary image pixels
-# def ray_light(img, name, dic, pos_light):
-#     #insert rays in a new collection
-#     for i in range(len(img)):
-#         for j in range(len(img[i])):
+# def ray_light(img, name, dic, pos_light, corners):
 #             # light position
 #             init_coord = [pos_light[0], pos_light[1], pos_light[2]]
-#             # pixel center position
-#             end_coord = [dic[(i,j)][0],dic[(i,j)][1],dic[(i,j)][2]]
+#             # boundary piyel positions
+#             end_coord_bl = [dic[(0,0)][0],dic[(0,0)][1],dic[(0,0)][2]]
+#             end_coord_br = [dic[(0,img_size-1)][0],dic[(0,img_size-1)][1],dic[(0,img_size-1)][2]]
+#             end_coord_tl = [dic[(img_size-1,0)][0],dic[(img_size-1,0)][1],dic[(img_size-1,0)][2]]
+#             end_coord_tr = [dic[(img_size-1, img_size-1)][0],dic[(img_size-1, img_size-1)][1],dic[(img_size-1, img_size-1)][2]]
 
+#             end_coords = [end_coord_bl, end_coord_br, end_coord_tl, end_coord_tr]
+
+#             info = [name,init_coord]
 
 #             # create lines from the light point to the center of each pixel. Increase then the length of the lines to know the space
 #             # to build the hull
-#             end_point =increase_line_length(init_coord, end_coord,30)
-#             verts = [(init_coord), (end_point[0], end_point[1], end_point[2])]
-#             edges = [(0, 1)]
-#             ray_light = bpy.data.meshes.new('ray_' + str(name) + '_' +str(i) + str(j))
-#             ray_light.from_pydata(verts, edges, [])
-#             ray_light.update()
-#             mesh_obj = bpy.data.objects.new('obj_' + str(name) + '_' + str(i) + str(j), ray_light)
-#             lights.objects.link(mesh_obj)
+#             for i in range(len(end_coords)):
+#                 end_point =increase_line_length(init_coord, end_coords[i],30)
+#                 info.append(end_point)
+#                 verts = [(init_coord), (end_point[0], end_point[1], end_point[2])]
+#                 edges = [(0, 1)]
+#                 ray_light = bpy.data.meshes.new('ray' + str(name))
+#                 ray_light.from_pydata(verts, edges, [])
+#                 ray_light.update()
+#                 mesh_obj = bpy.data.objects.new('obj_' + str(name), ray_light)
+#                 lights.objects.link(mesh_obj)
 
-def ray_light(img, name, dic, pos_light, corners):
-            # light position
-            init_coord = [pos_light[0], pos_light[1], pos_light[2]]
-            # boundary piyel positions
-            end_coord_bl = [dic[(0,0)][0],dic[(0,0)][1],dic[(0,0)][2]]
-            end_coord_br = [dic[(0,img_size-1)][0],dic[(0,img_size-1)][1],dic[(0,img_size-1)][2]]
-            end_coord_tl = [dic[(img_size-1,0)][0],dic[(img_size-1,0)][1],dic[(img_size-1,0)][2]]
-            end_coord_tr = [dic[(img_size-1, img_size-1)][0],dic[(img_size-1, img_size-1)][1],dic[(img_size-1, img_size-1)][2]]
-
-            end_coords = [end_coord_bl, end_coord_br, end_coord_tl, end_coord_tr]
-
-            info = [name,init_coord]
-            corners.append(info)
-
-            # create lines from the light point to the center of each pixel. Increase then the length of the lines to know the space
-            # to build the hull
-            for i in range(len(end_coords)):
-                end_point =increase_line_length(init_coord, end_coords[i],30)
-                info.append(end_point)
-                verts = [(init_coord), (end_point[0], end_point[1], end_point[2])]
-                edges = [(0, 1)]
-                ray_light = bpy.data.meshes.new('ray' + str(name))
-                ray_light.from_pydata(verts, edges, [])
-                ray_light.update()
-                mesh_obj = bpy.data.objects.new('obj_' + str(name), ray_light)
-                lights.objects.link(mesh_obj)
+#             corners.append(info)
 
 
-def increase_line_length(start_point, end_point, length_increase):
-    # Calculate direction vector of the line
-    direction = [(end_point[i] - start_point[i]) for i in range(3)]
+# def increase_line_length(start_point, end_point, length_increase):
+#     # Calculate direction vector of the line
+#     direction = [(end_point[i] - start_point[i]) for i in range(3)]
     
-    # Calculate current length of the line
-    current_length = math.sqrt(sum([direction[i] ** 2 for i in range(3)]))
+#     # Calculate current length of the line
+#     current_length = math.sqrt(sum([direction[i] ** 2 for i in range(3)]))
     
-    # Calculate scaling factor to increase the line length
-    scaling_factor = (current_length + length_increase) / current_length
+#     # Calculate scaling factor to increase the line length
+#     scaling_factor = (current_length + length_increase) / current_length
     
-    # Scale direction vector
-    scaled_direction = [direction[i] * scaling_factor for i in range(3)]
+#     # Scale direction vector
+#     scaled_direction = [direction[i] * scaling_factor for i in range(3)]
     
-    # Calculate new end point of the line
-    new_end_point = [(start_point[i] + scaled_direction[i]) for i in range(3)]
+#     # Calculate new end point of the line
+#     new_end_point = [(start_point[i] + scaled_direction[i]) for i in range(3)]
     
-    return new_end_point
+#     return new_end_point
 
 
 # fins total hull square space
-def hull_space(imgs):
-    # find all point intersection between all pair of rays
-    p_intersect = []
-    #pick current element and next one
-    for i in range(len(imgs)-1):
-        for j in range(i+1, len(imgs)):
-            p1_start = imgs[i][1] 
-            p2_start = imgs[j][1] 
-            # enter in each end point and check intersection with each pair
-            for k in range(2,len(imgs[i])):
-                for l in range(2,len(imgs[i])):
-                    p1_end = imgs[i][k]
-                    p2_end = imgs[j][l]
-                    point_intersection = find_intersection(p1_start,p1_end,p2_start,p2_end)
-                    if point_intersection != None:
-                        p_intersect.append(point_intersection)
-    print(p_intersect)
+def hull_space():
+    
+    
             
 
 
 
-def find_intersection(p1_start, p1_end, p2_start, p2_end):
-    x1, y1, z1 = p1_start
-    x2, y2, z2 = p1_end
-    x3, y3, z3 = p2_start
-    x4, y4, z4 = p2_end
 
-    # Calculate the direction vectors of both lines
-    direction1 = (x2 - x1, y2 - y1, z2 - z1)
-    direction2 = (x4 - x3, y4 - y3, z4 - z3)
-
-    # Calculate the cross product of the direction vectors
-    cross_product = (direction1[1] * direction2[2] - direction1[2] * direction2[1],
-                     direction1[2] * direction2[0] - direction1[0] * direction2[2],
-                     direction1[0] * direction2[1] - direction1[1] * direction2[0])
-    
-    # Calculate the determinant of the coefficient matrix
-    determinant = cross_product[0] * cross_product[0] + cross_product[1] * cross_product[1] + cross_product[2] * cross_product[2]
-
-    # Check if the lines are parallel (determinant == 0)
-    if determinant == 0:
-        return None
-    
-    # Calculate the difference in starting points
-    start_difference = (x3 - x1, y3 - y1, z3 - z1)
-
-    # Calculate the scale factor for the first line
-    scale_factor1 = (direction2[1] * start_difference[2] - direction2[2] * start_difference[1]) / \
-                    (direction1[0] * direction2[1] - direction1[1] * direction2[0])
-
-    # Calculate the scale factor for the second line
-    scale_factor2 = (start_difference[0] * direction1[1] - start_difference[1] * direction1[0]) / \
-                    (direction1[0] * direction2[1] - direction1[1] * direction2[0])
-
-    # Calculate the coordinates of the intersection point
-    intersection_x = x1 + scale_factor1 * direction1[0]
-    intersection_y = y1 + scale_factor1 * direction1[1]
-    intersection_z = z1 + scale_factor1 * direction1[2]
-
-    # Return the coordinates of the intersection point as a tuple (x, y, z)
-    return intersection_x, intersection_y, intersection_z
 
 
 # clean scene removing all collections and objects before working
@@ -278,18 +204,18 @@ dic_img4 = {}
 # for i in range(nr_img):
 create_img('img1',img1,dic_img1, math.radians(90))
 create_img('img2',img2,dic_img2,  math.radians(90))
-# create_img('img3',img3,dic_img3,  math.radians(90))
+create_img('img3',img3,dic_img3,  math.radians(90))
 
 for i in range(len(angles)):
     create_light(pos_lights[i])
 
 
-# array containing name of the image the rays belong to and starting_point(light), ending_point1,ending_point2... of corner pixels for each img
-corners_coord = []
+# # array containing name of the image the rays belong to and starting_point(light), ending_point1,ending_point2... of corner pixels for each img
+# corners_coord = []
 
-ray_light(img1,'img1', dic_img1, pos_lights[0], corners_coord)
-ray_light(img2,'img2',dic_img2, pos_lights[1], corners_coord)
+# ray_light(img1,'img1', dic_img1, pos_lights[0], corners_coord)
+# ray_light(img2,'img2',dic_img2, pos_lights[1], corners_coord)
 # ray_light(img3,'img3',dic_img3, pos_lights[2], corners_coord)
 
-hull_space(corners_coord)
+hull_space()
 
